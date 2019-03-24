eea8e77df7b19253b820236e27e23dc1c75e1013
Shawn Landden <shawn@git.icu>
2019-03-23 20:49:10 -0500

    breaking: big unicode overhaul
    Allow utf-8 in character literals
    
    Char Unicode escape syntax.
    
    Validate zig as UTF-8.
    
    overhaul std.unicode
    
    Fully implemented in stage2.
    
    Closes: #2097
    Closes: #2129
    ---
    
    About the UTF-8 validation in stage1: This implementation is quite slow,
    but the stage automata it claims to represent is correct,
    and it has two features faster validators don't that would
    make the code in stage1 more complicated:
    
    * They don't provide the char point
    * They don't provide the index of the error (although this could be
      hacked in, but at more cost)
    
    I don't want to put that much optimization effort into stage1 and C
    code.

--- std/zig/parse_string_literal.zig
+++ std/zig/parse_string_literal.zig
@@ -1,16 +1,94 @@
-const std = @import("../std.zig");
+const std = @import("std");//("../std.zig");
 const assert = std.debug.assert;
+const mem = std.mem;
+const unicode = std.unicode;
+const ascii = std.ascii;
+
+pub const ParseEscapeError = std.unicode.UnicodeError || error{
+    ExpectXDigit,
+    ExpectLCurly,
+    ExpectRCurly,
+};
+inline fn parseEscape(escape_sequence: []const u8, ret_len: *u4) ParseEscapeError!u21 {
+    var ret: u21 = undefined;
+    var it = mem.byteIterator(escape_sequence);
+    errdefer ret_len.* = it.i;
+    got_escape: { switch (it.n()) {
+    'x' => {
+        const hi = ascii.charToDigit(it.n(), 16) catch return error.ExpectXDigit;
+        const lo = ascii.charToDigit(it.n(), 16) catch return error.ExpectXDigit;
+        ret_len.* = 3;
+        return u21(((hi << 4) | lo));
+    },
+    'u' => {
+        if (it.n() != '{') return error.ExpectLCurly;
+        const hi = ascii.charToDigit(it.n(), 16) catch return error.ExpectXDigit;
+        const lo = ascii.charToDigit(it.n(), 16) catch return error.ExpectXDigit;
+        ret_len.* = 4;
+        ret = (hi << 4) | lo;
+        hi = ascii.charToDigit(it.n(), 16) catch {
+            if (it.n() != '}') return error.ExpectRCurly;
+            ret_len.* = 5;
+            break :got_escape;
+        };
+        lo = ascii.charToDigit(it.n(), 16) catch return error.ExpectXDigit;
+        ret_len.* = 6;
+        ret |= ((hi << 4) | lo) << 8;
+        hi = ascii.charToDigit(it.n(), 16) catch {
+            if (it.n() != '}') return error.ExpectRCurly;
+            ret_len.* = 7;
+            break :got_escape;
+        };
+        lo = ascii.charToDigit(it.n(), 16) catch return error.ExpectXDigit;
+        ret_len.* = 8;
+        ret |= ((hi << 4) | lo) << 16;
+        if (it.n() != '}') return error.ExpectRCurly;
+        ret_len.* = 9;
+    },
+    else => unreachable,
+    }}
+    unicode.isValidUnicode(ret) catch |err| return err;
+    return ret;
+}
+
+pub const ParseCharLiteralError = ParseEscapeError || error{
+    ExpectSQuote,
+};
+pub fn parseCharLiteral(char_token: []const u8) ParseCharLiteralError!u21 {
+    var char: u21 = undefined;
+    if (char_token[1] == '\\') {
+        var len: u4 = undefined;
+        char = switch (char_token[2]) {
+        'x', 'u' => try parseEscape(char_token[2..], &len),
+        'n' => '\n',
+        'r' => '\r',
+        '\\' => '\\',
+        '\t' => '\t',
+        '\'' => '\'',
+        '\"' => '\"',
+        else => unreachable,
+        };
+        if (char_token[2 + len] != '}') return error.ExpectRCurly;
+    }
+    char = char_token[2];
+    if (char_token[3] != '\'') return error.ExpectSQuote;
+
+    return char;
+}
+
+test "zig.parseCharLiteral" {
+    const expect = std.testing.expect;
+    expect(parseCharLiteral("\'0\'") catch unreachable == '0');
+    expect(parseCharLiteral("\'\x20\'") catch unreachable == ' ');
+}
 
 const State = enum {
     Start,
     Backslash,
 };
 
-pub const ParseStringLiteralError = error{
+pub const ParseStringLiteralError = ParseEscapeError || error{
     OutOfMemory,
-
-    /// When this is returned, index will be the position of the character.
-    InvalidCharacter,
 };
 
 /// caller owns returned memory
@@ -29,7 +107,9 @@ pub fn parseStringLiteral(
     try list.ensureCapacity(slice.len - 1);
 
     var state = State.Start;
-    for (slice) |b, index| {
+    var index: usize = 0;
+    while (index < slice.len) : (index += 1) {
+        var b = slice[index];
         switch (state) {
             State.Start => switch (b) {
                 '\\' => state = State.Backslash,
@@ -41,9 +121,15 @@ pub fn parseStringLiteral(
                 else => try list.append(b),
             },
             State.Backslash => switch (b) {
-                'x' => @panic("TODO"),
-                'u' => @panic("TODO"),
-                'U' => @panic("TODO"),
+                'x', 'u' => {
+                    var encoded: [4]u8 = undefined;
+                    var len: u3 = undefined;
+                    bad_index.* = index;
+                    len = unicode.utf8Encode(try parseEscape(char_token[2..], &len), encoded[0..]) catch unreachable;
+                    try list.appendSlice(encoded[0..len]);
+                    index += len;
+                    state = State.Start;
+                },
                 'n' => {
                     try list.append('\n');
                     state = State.Start;
@@ -62,6 +148,10 @@ pub fn parseStringLiteral(
                 },
                 '"' => {
                     try list.append('"');
+                    state = State.Start;
+                },
+                '\'' => {
+                    try list.append('\'');
                     state = State.Start;
                 },
                 else => {
